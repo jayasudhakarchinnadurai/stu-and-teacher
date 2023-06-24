f187e26ed52b743396d762d5d09ee56f75d4225e
jayasudhakarchinnadurai <jayasudhakar183@gamil.com>
2023-06-24 13:23:34 +0530

    change "m"

--- src/Student/stuedit.js
+++ src/Student/stuedit.js
@@ -1,26 +1,58 @@
 import React from "react";
-// import { useParams } from "react-router-dom/cjs/react-router-dom";
-// // import { useEffect } from "react";
-
-// import { useState } from "react";
-
-import {useParams}from "react-router-dom";
-// import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 
 function EditStudent({user, setuser}){
-   const {id}=useParams();
-   const per=user[id]
-    
+   const history=useHistory();
+   const {id}=useParams()
+   console.log(id)
+   const selectuser= user.find((per,indx)=>per.id === id);
+    console.log(selectuser);
+ 
+   const [idx,setidx]=useState();
+   const[name,setname]=useState();
+   const[email,setemail]=useState();
+   const[batch, setbatch]=useState();
+   const[year, setyear]=useState();
+   
+
+
+
+   
    return(
       <div>
-         <input
-          value={per.id}
+         <input 
+         placeholder="id"
+         
+         value={idx}
+         onChange={(event)=>setidx(event.target.value)}
+         
+         />
+         <input 
+         placeholder="name"
+         value={name}
+         onChange={(event)=>setname(event.target.value)}
+         />
+         <input placeholder="email"
+          onChange={(event)=>setemail(event.target.value)}
+         value={email}
+         />
+         <input 
+         placeholder="batch"
+         onChange={(event)=>setbatch(event.target.value)}
+         value={batch}
+         />
+         <input placeholder="year"
+          onChange={(event)=>setyear(event.target.value)}
+           value={year}
          />
 
+         <button >editstudent</button>
       </div>
+
    )
 }
-
 
 export default EditStudent;
--- src/Student/viewstu.js
+++ src/Student/viewstu.js
@@ -1,90 +1,20 @@
 import React from "react";
-import { useEffect } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
-import { useState } from "react";
-
-import {useParams}from "react-router-dom";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
-
-function EditStudent({user, setuser}){
-    const history=useHistory()
-
-    const {id}=useParams()
-    console.log(id)
-   
-    const [idx, setidx]=useState("");
-    const [name, setname]=useState("");
-    const [batch, setbatch]=useState("");
-    const [email, setemail]=useState("");
-    const [exprience, setexprience]=useState("");
-
-    const selectuser=user.find((per)=>per.id === id)
-    
-
-    useEffect(()=>{
-        setidx(selectuser.id)
-        setname(selectuser.name)
-        setbatch(selectuser.batch)
-        setemail(selectuser.email)
-        setexprience(selectuser.exprience)
-    },[])
-
-    const updateuser =()=>{
-
-        const editindex=user.findIndex((per)=>per.id === id)
-        console.log(editindex)
-       
-        const editdata={
-            idx,
-            name, 
-            batch,
-            email,
-            exprience
-        }
-        console.log(editdata)
-        user[editindex] =editdata;
-        setuser([...user])
-        history.push("/")
-    }
-    
-  
-   return (
-    <div>
-       
-
-
-        <input
-         placeholder="id"
-         value={idx}
-        onChange={(event)=>setidx(event.target.value)}
-         />
-        <input 
-        placeholder="name"
-        value={name}
-        onChange={(event)=>setname(event.target.value)}
-        />
-    
-        <input 
-        placeholder="batch"
-        value={batch}
-        onChange={(event)=>setbatch(event.target.value)}
-        />
-        <input  
-        placeholder="email"
-        value={email}
-        onChange={(event)=>setemail(event.target.value)}
-        />
-        <input
-         placeholder="exprience"
-        value={exprience}
-        onChange={(event)=>setexprience(event.target.value)}
-        />
-        <button onClick={updateuser}>Edituser</button>
-    
-    </div>
-   )
+function Viewstudent({user}){
+    const history=useHistory();
+    const {id}=useParams();
+    const per=user[id]
+    return(
+        <div>
+            <button onClick={()=>history.push('/student')}>Studentprofile</button>
+         <h1 style={{ color: 'red',  }}>Student Details</h1>   
+         <h3>{per.name}</h3>
+         <p>{per.email}</p>
+          <p>{per.year}</p>
+        </div>
+    )
 }
 
-
-export default EditStudent;
+export default Viewstudent;
